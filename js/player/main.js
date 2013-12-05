17777ae5eb888ee52b01acc16cbbac8eeba3854a
Dan Kantor <dan.kantor@gmail.com>
2013-12-04 20:13:20 -0500

    hooked up some basic player controls

--- js/player/main.js
+++ js/player/main.js
@@ -5,27 +5,50 @@ function Main(){
             'audio': this.audio,
             'use_local_storage': false
         }
-    )
+    );
+    this.playQueue.savedSongProperties = [
+        'title', 'artist', 'album',
+        'artwork', 'url', 'link',
+        'timestamp', 'purchaseUrl', 'type',
+        'originalType', 'originalSource', 'duration',
+        'serviceId', 'postAuthor'
+    ];
     chrome.runtime.sendMessage(null, 
         {
             "type": 'deepScan'
         }
     )
-    $(document.body).on('click', function(){
+    $('#minimize').on('click', function(){
         chrome.runtime.sendMessage(null, 
             {
                 "type": 'minimize'
             }
         )   
     })
+    $('#play-pause').on('click', function(){
+        this.playQueue.playPause();  
+    }.bind(this));
+    $('#prev').on('click', function(){
+        this.playQueue.previous();  
+    }.bind(this));
+    $('#next').on('click', function(){
+        this.playQueue.next();  
+    }.bind(this))
 }
 
 Main.prototype.gotPlaylist = function(list){
     this.playQueue.add(list);
     this.playQueue.play(0);
-    console.log('playQueue', this.playQueue.getList());
+    this.playQueue.addEventListener(
+        "playing", 
+        function(e){
+            console.log(e);
+            $('#song-title').text(e.target.song.title);
+        }.bind(this), 
+        false
+    );
+    
     var template = document.getElementById('song');
-    //console.log(template.content, template.innerHTML);
     document.body.appendChild(template.content.cloneNode(true));
 }
 var main;
