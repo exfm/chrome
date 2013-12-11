3027677e8a984e59b6486ac4671b71b743356de3
Dan Kantor <dan.kantor@gmail.com>
2013-12-11 13:32:41 -0500

    lastfm nowPlaying and scrobble now using lastfm oauth plugin

--- js/oauth/jquery.oauth.js
+++ js/oauth/jquery.oauth.js
@@ -23,13 +23,38 @@ $.oauth = function(options) {
 (function ($){
 
 $.oauthLastfm = function(options) {
-    var paramString = OAuth.SignatureMethod.normalizeParameters(options.data);
-    paramString = paramString.replace(/\=/g, "");
-    paramString = paramString.replace(/\&/g, "");
+    var list = OAuth.getParameterList(options.data);
+    var sortable = [];
+    for (var p = 0; p < list.length; ++p) {
+        var nvp = list[p];
+        sortable.push([ nvp[0]
+                          + " "
+                          + nvp[1]
+                          , nvp]);
+    }
+    sortable.sort(function(a,b) {
+                      if (a[0] < b[0]) return  -1;
+                      if (a[0] > b[0]) return 1;
+                      return 0;
+                  });
+    var sorted = [];
+    for (var s = 0; s < sortable.length; ++s) {
+        sorted.push(sortable[s][1]);
+    }
+    
+    var paramString = "";
+    for (var p = 0; p < sorted.length; ++p) {
+        var value = sorted[p][1];
+        if (value == null){ 
+            value = "";
+        }
+        paramString += sorted[p][0] + value;
+    }
     paramString += options.consumerSecret;
     var apiSignature = hex_md5(paramString);
     options.data.api_sig = apiSignature;
-    return $.ajax(options);    
+    return $.ajax(options);
+   
 };
 
 }($));
